ae9fb20d9101ed7ddac7e89f143e86e00981a456
Swaraj Mohite <156696990+SwarajMohite@users.noreply.github.com>
2024-11-22 14:00:05 +0530

    Create comments.sql

---
+++ Codes/comments.sql
@@ -0,0 +1,29 @@
+-- All the Comments on the table 
+
+comment on table Starter 
+is 'Details of all Starter in Menu';
+
+comment on table Breakfast
+is 'Details of all Breakfast in Menu';
+
+
+comment on table Lunch
+is 'Details of all Lunch in Menu';
+
+
+comment on table Dinner
+is 'Details of all Dinner in Menu';
+
+
+comment on table Dessert
+is 'Details of all Dessert in Menu';
+
+
+comment on table Employee
+is 'Details of all Employees from our Hotel';
+
+
+comment on table Customer
+is 'Details of all Customers that had visited US';
+
+
