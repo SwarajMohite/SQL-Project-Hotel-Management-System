2625486869e927215d0d97004b8efaff1a4fb177
Swaraj Mohite <156696990+SwarajMohite@users.noreply.github.com>
2024-11-22 13:08:25 +0530

    Create Starter-DML.sql

---
+++ Codes/Starter-DML.sql
@@ -0,0 +1,85 @@
+-- Inserting random data for Starter
+
+INSERT INTO Starter(id, name, price, stars)
+VALUES
+(id.nextval, 'Samosa', 50.00, '4');
+
+INSERT INTO Starter(id, name, price, stars)
+VALUES
+(id.nextval, 'Pani Puri', 40.00, '5');
+
+INSERT INTO Starter(id, name, price, stars)
+VALUES
+(id.nextval, 'Pakora', 60.00, '3');
+
+INSERT INTO Starter(id, name, price, stars)
+VALUES
+(id.nextval, 'Dhokla', 30.00, '4');
+
+INSERT INTO Starter(id, name, price, stars)
+VALUES
+(id.nextval, 'Aloo Tikki', 50.00, '4');
+
+INSERT INTO Starter(id, name, price, stars)
+VALUES
+(id.nextval, 'Vegetable Spring Roll', 80.00, '4');
+
+INSERT INTO Starter(id, name, price, stars)
+VALUES
+(id.nextval, 'Chili Paneer', 120.00, '5');
+
+INSERT INTO Starter(id, name, price, stars)
+VALUES
+(id.nextval, 'Onion Bhaji', 70.00, '3');
+
+INSERT INTO Starter(id, name, price, stars)
+VALUES
+(id.nextval, 'Pav Bhaji', 90.00, '4');
+
+INSERT INTO Starter(id, name, price, stars)
+VALUES
+(id.nextval, 'Vada Pav', 40.00, '5');
+
+INSERT INTO Starter(id, name, price, stars)
+VALUES
+(id.nextval, 'Hara Bhara Kebab', 70.00, '4');
+
+INSERT INTO Starter(id, name, price, stars)
+VALUES
+(id.nextval, 'Methi Malai Murg', 150.00, '5');
+
+INSERT INTO Starter(id, name, price, stars)
+VALUES
+(id.nextval, 'Corn and Cheese Balls', 90.00, '4');
+
+INSERT INTO Starter(id, name, price, stars)
+VALUES
+(id.nextval, 'Aloo Chaat', 50.00, '5');
+
+INSERT INTO Starter(id, name, price, stars)
+VALUES
+(id.nextval, 'Dhokla Chaat', 65.00, '4');
+
+INSERT INTO Starter(id, name, price, stars)
+VALUES
+(id.nextval, 'Pesarattu', 80.00, '3');
+
+INSERT INTO Starter(id, name, price, stars)
+VALUES
+(id.nextval, 'Cheese Pakoda', 100.00, '4');
+
+INSERT INTO Starter(id, name, price, stars)
+VALUES
+(id.nextval, 'Vegetable Sizzler', 120.00, '5');
+
+INSERT INTO Starter(id, name, price, stars)
+VALUES
+(id.nextval, 'Sambhar Vada', 80.00, '4');
+
+INSERT INTO Starter(id, name, price, stars)
+VALUES
+(id.nextval, 'Momos', 120.00, '5');
+
+-- View whole Starter 
+
+select * from Starter
